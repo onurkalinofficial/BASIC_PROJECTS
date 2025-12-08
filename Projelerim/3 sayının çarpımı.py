828e7ddc24d9cfefad5a7704aae92388b1ebe746
onurkalinofficial <physicist.dr.onur@gmail.com>
2025-12-08 20:02:40 +0300

    yeni program(3 sayının çarpımı) eklendi.

---
+++ Projelerim/3 sayının çarpımı.py
@@ -0,0 +1,7 @@
+sayı1 = int(input("ilk sayıyı giriniz: "))
+sayı2 = int(input("ikinci sayıyı giriniz: "))
+sayı3 = int(input("üçüncü sayıyı giriniz: "))
+
+çarpım = sayı1*sayı2*sayı3
+
+print("sayıların çarpımı: ",çarpım,)
