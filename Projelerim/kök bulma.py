9e431bfc149d9dac9c61dec3c6aaba3c1ad2aa04
onurkalinofficial <physicist.dr.onur@gmail.com>
2025-12-08 19:51:43 +0300

    Initial commit

---
+++ Projelerim/kök bulma.py
@@ -0,0 +1,10 @@
+a = int(input("a sayısını gir: "))
+b = int(input("b sayısını gir: "))
+c = int(input("c sayısını gir: "))
+delta = b**2 - 4*a*c
+kök1 = -b-delta**0.5/(2*a)
+kök2 = -b+delta**0.5/(2*a)
+
+print("kök1: {}\nkök2: {}".format([kök1],[kök2]))
+
+
---
+++ Projelerim/oyuncu_kaydetme.py
@@ -0,0 +1,12 @@
+print("Hoş Geldiniz...")
+isim = input("isim giriniz: ")
+soyisim = input("soyisim giriniz: ")
+takım = input("takım giriniz: ")
+
+bilgiler = [isim,soyisim,takım]
+
+print("bilgiler kayıt ediliyor, bekleyiniz...")
+
+print("isim: {}\nsoyisim: {}\ntakım: {}\n".format(bilgiler[0],bilgiler[1],bilgiler[2]))
+
+print("Bilgileriniz Kayıt Edilmiştir.")
