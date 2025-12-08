bd81c071582542a302d81a3ca26fbf816432dda5
onurkalinofficial <physicist.dr.onur@gmail.com>
2025-12-08 22:36:08 +0300

    .

---
+++ Projects/sayı değerleri değiştirme.py
@@ -0,0 +1,16 @@
+print("---HOŞ GELDİNİZ---")
+a = int(input("a sayısını gir: "))
+b = int(input("b sayısını gir: "))
+
+print("değiştirilmeden önce sayılarınız: ")
+print("a= ",a,)
+print("b= ",b,)
+
+a,b = b,a
+
+print("değiştirildikten sonra sayılarınız: ")
+print("a= ",a,)
+print("b= ",b,)
+
+print("---HOŞÇAKALIN---")
+
